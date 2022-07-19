fd1cd23786e0cc2115eb82dd61ea487a851c6eb3
Tatiana Ignateva <tatyanazhzh@mail.ru>
2022-07-19 21:08:43 +0300

    Пропущеное 3-е задание.

--- h2_5.py
+++ h2_5.py
@@ -2,17 +2,17 @@
 #     выполнить подсчёт строк и слов в каждой строке.
 
 
-with open('text_2.txt', 'r', encoding='utf-8') as f:
+with open('text_2.txt', 'r',  encoding='utf-8') as f:
     content = f.read()
     print(f'Содеожимое файла: \n{content}')
 
-with open('text_2.txt', 'r') as f:
+with open('text_2.txt') as f:
     len_content = f.readlines()
     print(f'Колличество строк в файле: \n{len(len_content)}')
 
 numb_words = 0
 numb_lines = 0
-with open('text_2.txt', 'r') as f:
+with open('text_2.txt') as f:
 
     for line in f:
         numb_lines += 1
