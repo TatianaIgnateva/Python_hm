9cad823becea7833bab3840392f258e173d667d4
Tatiana Ignateva <tatyanazhzh@mail.ru>
2022-07-19 20:59:04 +0300

    Пропущеное 3-е задание.

--- h6_3.py
+++ h6_3.py
@@ -1,3 +1,27 @@
+# 3. Реализовать базовый класс Worker (работник).
+# определить атрибуты: name, surname, position (должность), income (доход);
+# последний атрибут должен быть защищённым и ссылаться на словарь, содержащий элементы:
+# оклад и премия, например, {"wage": wage, "bonus": bonus};
+# создать класс Position (должность) на базе класса Worker;
+# в классе Position реализовать методы получения полного имени сотрудника (get_full_name)
+# и дохода с учётом премии (get_total_income);
+# проверить работу примера на реальных данных: создать экземпляры класса Position,
+# передать данные, проверить значения атрибутов, вызвать методы экземпляров.
 
+class Worker:
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self.incom = {"wage": wage, "bonus": bonus}
 
+class Position(Worker):
+    def get_full_name(self):
+        return self.name + ' ' + self.surname
+    def get_full_salary(self):
+        return self.incom["wage"] + self.incom["bonus"]
 
+positioner = Position("Ray", "Ban", "disigner", 50_000, 100_000)
+print(positioner.get_full_name())
+print(positioner.position)
+print(positioner.get_full_salary())
